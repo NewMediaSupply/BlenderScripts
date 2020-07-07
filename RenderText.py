d6e35a0aaf586f435501798b0a42cd953f76e633
NewMediaSupply <67974774+NewMediaSupply@users.noreply.github.com>
2020-07-08 00:38:16 +0100

    Update RenderText.py
    add newly created objects in container

--- RenderText.py
+++ RenderText.py
@@ -1,8 +1,16 @@
 import bpy
 
 bpy.ops.object.text_add(enter_editmode=False, align='WORLD', location=(0, 0, 0))
-bpy.context.object.data.align_x = 'CENTER'
-bpy.context.object.data.align_y = 'CENTER'
+# Instead of continue writing like this: 
+# bpy.context.object.data.align_x = 'CENTER'
+# bpy.context.object.data.align_y = 'CENTER'
+# You better put the now active object, the text, in a container. Like this: 
+
+tex = bpy.context.active_object
+tex.data.align_x = 'CENTER'
+tex.data.align_y = 'CENTER'
+
+# So that way we can always adress the object called tex.  
 
 obj_camera = bpy.context.scene.camera
 obj_camera.location = (0,0,5)
